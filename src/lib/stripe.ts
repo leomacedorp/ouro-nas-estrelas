10bda4355818e834396dadc9d9a412862197bda6
Leonardo Antonio de Macedo <infohausti@gmail.com>
2026-01-29 13:10:00 -0300

    fix: remove Stripe API version to use SDK default

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,9 +3,7 @@ import Stripe from 'stripe';
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 
 export const stripe = stripeSecretKey
-    ? new Stripe(stripeSecretKey, {
-        apiVersion: '2025-12-15.clover',
-    })
+    ? new Stripe(stripeSecretKey)
     : null;
 
 export function getStripe(): Stripe {
